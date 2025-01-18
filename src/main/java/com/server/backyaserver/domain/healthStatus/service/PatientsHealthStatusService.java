08da81a780eb2754df4200a3e32bb4dec8fafd92
ralph <sanghwaj222@gmail.com>
2025-01-19 03:10:13 +0900

    [feat/#31] fetch and merge before API complete

--- src/main/java/com/server/backyaserver/domain/healthStatus/service/PatientsHealthStatusService.java
+++ src/main/java/com/server/backyaserver/domain/healthStatus/service/PatientsHealthStatusService.java
@@ -1,7 +1,9 @@
 package com.server.backyaserver.domain.healthStatus.service;
 
+import com.server.backyaserver.domain.healthStatus.domain.HealthStatus;
 import com.server.backyaserver.domain.healthStatus.dto.HealthStatusResponse;
 import com.server.backyaserver.domain.healthStatus.repository.PatientsHealthStatusRepository;
+import com.server.backyaserver.global.error.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,5 +20,14 @@ public class PatientsHealthStatusService {
         return patientsHealthStatusRepository.findAllByPatientId(id).stream()
                 .map(HealthStatusResponse::of)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public HealthStatusResponse getPatientsHealthStatusById(Long statusId, Long patientId) {
+
+        HealthStatus healthStatus = patientsHealthStatusRepository.findByIdAndPatientId(statusId, patientId).
+                orElseThrow(() -> new NotFoundException(ErrorCode."Patient not found"));
+        ;
+        return HealthStatusResponse.of(healthStatus);
     }
 }
